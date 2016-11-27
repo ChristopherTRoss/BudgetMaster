ca086e617dbee69e7d4e3e4b4a11d9c05d49b0d1
adrian-colon <acolon5@lsu.edu>
2016-11-26 22:43:12 -0600

    added cardview to Home page

--- app/src/main/java/com/budgetmaster/budgetmaster/HomeFragment.java
+++ app/src/main/java/com/budgetmaster/budgetmaster/HomeFragment.java
@@ -2,6 +2,7 @@ package com.budgetmaster.budgetmaster;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +14,10 @@ import android.widget.TextView;
  */
 
 public class HomeFragment extends Fragment{
-    //todo retrieve recent transactions from DB, store here
-    private String[] recentTransactions;
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +26,14 @@ public class HomeFragment extends Fragment{
         View inflatedView = inflater.inflate(R.layout.home_fragment, container, false);
         TextView spendInc = (TextView) inflatedView.findViewById(R.id.spendable_income);
         spendInc.setText(String.format("$%.2f", (double) MainActivity.spendableInc));
+
+        //Todo load categories of transactions from DB, temp data now
+        String[] home_categories = {"Food", "Gas", "Rent", "Miscellaneous", "Utilities"};
+        mRecyclerView = (RecyclerView) inflatedView.findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new HomeRecyclerAdapter(home_categories);
+        mRecyclerView.setAdapter(mAdapter);
 
         return inflatedView;
     }
