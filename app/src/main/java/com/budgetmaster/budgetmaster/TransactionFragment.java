dcfae1defa91dfc39d72f197a6bb075e606fc7ff
atho219 <atho219@lsu.edu>
2016-11-27 14:51:30 -0600

    Implemented sorting menu

--- app/src/main/java/com/budgetmaster/budgetmaster/TransactionFragment.java
+++ app/src/main/java/com/budgetmaster/budgetmaster/TransactionFragment.java
@@ -6,24 +6,30 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /****************************************************************************************/
 /*
 /* FILE NAME: TransactionFragment
 /*
 /* DESCRIPTION: The TransactionFragment class.
- */
-
+/*
+/*
 /*
 /* REFERENCE:
 /*
 /* DATE         BY             CHANGE REF         DESCRIPTION
 /* ========    =============    ===========       =============
 /* 11/12/2016  Jason Williams   72CF: LE          Created the class.
-/*
-/*
+/* 11/26/2016  Adrian Colon                       Implemented recycler view
+/* 11/27/2016  Ross Thompson                      Added spinner menu functionality for selecting
+/*                                                sorting methods from menu
 /*
 /*
 /****************************************************************************************/
@@ -46,6 +52,7 @@ public class TransactionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         View inflatedView = inflater.inflate(R.layout.transaction_fragment, container, false);
 
         //RecyclerView recList = (RecyclerView) inflatedView.findViewById(R.id.transactionList);
@@ -65,9 +72,50 @@ public class TransactionFragment extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new TransactionRecyclerAdapter(transaction_titles, transaction_dates, transaction_prices);
         mRecyclerView.setAdapter(mAdapter);
-
-
         return inflatedView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.sortSpinner).setVisible(true);
+        super.onPrepareOptionsMenu(menu);
+        //populates the spinner menu
+        MenuItem item = menu.findItem(R.id.sortSpinner);
+        Spinner spinner = (Spinner)item.getActionView();
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.sorting_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                if (pos == 0) {
+                    //replace this with SortDate
+                    Toast toast = Toast.makeText(getActivity(), "Sort by Date", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else if (pos == 1) {
+                    //replace withs SortAmount
+                    Toast toast = Toast.makeText(getActivity(), "Sort by Amount", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else if (pos == 2) {
+                    //replace with SortCategory
+                    Toast toast = Toast.makeText(getActivity(), "Sort by Category", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+            //Mandatory method, dont need so leaving empty
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
     }
 
     @Override
