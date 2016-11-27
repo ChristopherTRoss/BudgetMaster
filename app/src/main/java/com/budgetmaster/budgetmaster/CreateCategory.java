d903f8a3686ac2de786f44f2651b5528c02266c5
ghardy778 <ghardy3@lsu.edu>
2016-11-27 13:09:24 -0600

    Categories are now added to the db when created

--- app/src/main/java/com/budgetmaster/budgetmaster/CreateCategory.java
+++ app/src/main/java/com/budgetmaster/budgetmaster/CreateCategory.java
@@ -1,9 +1,11 @@
 package com.budgetmaster.budgetmaster;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,10 +13,22 @@ import android.widget.Toast;
 
 public class CreateCategory extends AppCompatActivity{
 
+    SQLiteDatabase db = null;
+    Database budDB = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_category);
+        try {
+            db = this.openOrCreateDatabase("budgetDB", MODE_PRIVATE, null);
+        }
+        catch(Exception e)
+        {
+            System.out.println("It got caught....");
+            Log.e("BudgetDatabase ERROR", "Error Creating/Loading database");
+        }
+        budDB = new Database(db);
+        budDB.createTables();
     }
 
     public void addCategory(View view) {
@@ -28,6 +42,12 @@ public class CreateCategory extends AppCompatActivity{
             double amount = Double.valueOf(amountView.getText().toString());
 
             //todo add to DB
+            try {
+                budDB.addCategory(title, "expense", amount);
+            }
+            catch(Exception e) {
+                Log.e("BudgetDatabase ERROR", "Category was not added to DB");
+            }
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
